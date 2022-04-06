b8b24b102980b23cfe5829eeb2f3c718a4b972a1
yoelpi <103085955+yoelpi@users.noreply.github.com>
2022-04-06 11:08:49 +0800

    Create 20220406_variabel.cpp

---
+++ 20220406_variabel.cpp
@@ -0,0 +1,12 @@
+//header
+#include <iostream>
+using namespace std;
+int main(){//begin of function
+    float angka;
+    string nama; //deklarasi variabel
+    nama = "Yoel Pratama"; //masukkan value ke dalam variabel
+    angka = 10.21+3.14;
+    cout<<nama<<endl; //menampilkan variabel
+    cout<<angka;
+    
+}//end of function
