4a24ed90476730de59ec8482d661813c0e2adf11
yoelpi <103085955+yoelpi@users.noreply.github.com>
2022-04-06 11:27:34 +0800

    Create 20220406_input_output.cpp

---
+++ 20220406_input_output.cpp
@@ -0,0 +1,10 @@
+//header
+#include <iostream>
+using namespace std;
+int main(){//begin of function
+    string nama;
+    cout<<"Masukkan Nama: "; cin>> nama;
+    cout<<"Nama Anda: "<<nama;
+
+    
+}//end of function
