690a4eba6b5b11d0be2296bd6c59a6bc221d4725
yoelpi <103085955+yoelpi@users.noreply.github.com>
2022-04-27 10:16:04 +0800

    Create 20220427whilept1.cpp

---
+++ 20220427whilept1.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int number1 = 0;
+    while (number1 ++ <10){ //perulangan naik
+        cout<<"1"<<" * "<<number1<<" = "<<number1*1<<endl;
+    }
+    cout<<endl<<endl;
+    
+    int number2 = 0;
+    while (number2 ++ <10){ //perulangan naik
+        cout<<"2"<<" * "<<number2<<" = "<<number2*2<<endl;
+    }
+    cout<<endl<<endl;
+    
+    int number3 = 0;
+    while (number3 ++ <10){ //perulangan naik
+        cout<<"3"<<" * "<<number3<<" = "<<number3*3<<endl;
+    }
+    cout<<endl<<endl;
+
+    int number4 = 0;
+    while (number4 ++ <10){ //perulangan naik
+        cout<<"4"<<" * "<<number4<<" = "<<number4*4<<endl;
+    }
+    cout<<endl<<endl;
+    
+    int number5 = 0;
+    while (number5 ++ <10){ //perulangan naik
+        cout<<"5"<<" * "<<number5<<" = "<<number5*5<<endl;
+    }
+    cout<<endl<<endl;
+    
+    int number6 = 0;
+    while (number6 ++ <10){ //perulangan naik
+        cout<<"6"<<" * "<<number6<<" = "<<number6*6<<endl;
+    }
+    cout<<endl<<endl;
+    
+    int number7 = 0;
+    while (number7 ++ <10){ //perulangan naik
+        cout<<"7"<<" * "<<number7<<" = "<<number7*7<<endl;
+    }
+    cout<<endl<<endl;
+    
+    int number8 = 0;
+    while (number8 ++ <10){ //perulangan naik
+        cout<<"8"<<" * "<<number8<<" = "<<number8*8<<endl;
+    }
+    cout<<endl<<endl;
+    
+    int number9 = 0;
+    while (number9 ++ <10){ //perulangan naik
+        cout<<"9"<<" * "<<number9<<" = "<<number9*9<<endl;
+    }
+    cout<<endl<<endl;
+    
+    int number10 = 0;
+    while (number10 ++ <10){ //perulangan naik
+        cout<<"10"<<" * "<<number10<<" = "<<number10*10<<endl;
+    }
+    cout<<endl<<endl;
+
+    return 0;
+}
