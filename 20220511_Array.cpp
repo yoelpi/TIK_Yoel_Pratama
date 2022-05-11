7933ad9e65b550ab4356513a3dd9f7cdc2ec8dcb
yoelpi <103085955+yoelpi@users.noreply.github.com>
2022-05-11 10:02:19 +0800

    Create 20220511_Array.cpp

---
+++ 20220511_Array.cpp
@@ -0,0 +1,12 @@
+//array
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    string nama[7] = {"David", "Kabila", "Bryan", "Sydney", "Gracia", "Keysie", "Jodi"};
+    for(int i= 0; i<7 ; i ++){
+    cout<<nama[i]<<endl;
+    }
+    return 0;
+}
