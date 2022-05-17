150994011634e93173f273fd905ad62c3d7ba606
yoelpi <103085955+yoelpi@users.noreply.github.com>
2022-05-17 15:11:07 +0800

    Create 20220511_Array3.cpp

---
+++ 20220511_Array3.cpp
@@ -0,0 +1,17 @@
+//std::array<T, N> ;
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    int a [2] [2] = {
+        {},
+        {},
+        
+    };
+    
+    for
+    cout<<int a [i][j] + b[i][j]
+    
+    return 0;
+}
