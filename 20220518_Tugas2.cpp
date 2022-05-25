80886c32915d1b0573e0a74babf9200597a06ed6
yoelpi <103085955+yoelpi@users.noreply.github.com>
2022-05-25 09:48:10 +0800

    Create 20220518_Tugas2.cpp

---
+++ 20220518_Tugas2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+Int main(){
+  Int x,y,z;
+  Cout<<"Masukkan nilai X = ";
+  Cin>>x;
+  Cout<<"Masukkan Nilai Y = ";
+  Cin>>y;
+  z = x;
+  x = y;
+  y = z;
+
+Cout<<"Nilai X adalah = "<<x;
+Cout<<endl;
+Cout<<"Nilai Y adalah = "<<y;
+
+}
